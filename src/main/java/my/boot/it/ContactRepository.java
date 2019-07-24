aa46574bdfbf4487bbb5e0a287390bd3cdbf0227
13891362942@wo.cn <13891362942@wo.cn>
2019-07-24 09:13:25 +0800

    做小部分修改

--- src/main/java/my/boot/it/ContactRepository.java
+++ src/main/java/my/boot/it/ContactRepository.java
@@ -3,11 +3,12 @@ package my.boot.it;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
-
+@Repository
 public class ContactRepository {
     private JdbcTemplate jdbc;
     @Autowired
@@ -18,17 +19,14 @@ public class ContactRepository {
         return jdbc.query(
                 "select id,firstName,lastName,phoneNumber,emailAddress" +
                         "from contacts order by lastName",
-                new RowMapper<Contact>() {
-                    @Override
-                    public Contact mapRow(ResultSet resultSet, int i) throws SQLException {
-                        Contact contact = new Contact();
-                        contact.setId(resultSet.getLong(1));
-                        contact.setFirstName(resultSet.getNString(2));
-                        contact.setLastName(resultSet.getString(3));
-                        contact.setPhoneNumber(resultSet.getString(4));
-                        contact.setEmailAddress(resultSet.getString(5));
-                        return contact;
-                    }
+                (resultSet, i) -> {
+                    Contact contact = new Contact();
+                    contact.setId(resultSet.getLong(1));
+                    contact.setFirstName(resultSet.getNString(2));
+                    contact.setLastName(resultSet.getString(3));
+                    contact.setPhoneNumber(resultSet.getString(4));
+                    contact.setEmailAddress(resultSet.getString(5));
+                    return contact;
                 }
         );
     }
--- src/main/java/my/boot/it/ItApplication.java
+++ src/main/java/my/boot/it/ItApplication.java
@@ -1,13 +1,11 @@
 package my.boot.it;
-
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+@ComponentScan
+@EnableAutoConfiguration
 public class ItApplication {
-
     public static void main(String[] args) {
-
         SpringApplication.run(ItApplication.class, args);
     }
 
